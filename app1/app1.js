4f843b89dbed041bd37e0779111e38ededec78c0
unknown <system.nathy@hotmail.com>
2021-05-24 16:04:34 -0400

    first commit

---
+++ app1/app1.js
@@ -0,0 +1,14 @@
+
+//operador de soma 
+console.log(40+4);
+
+//operador de subtraçao
+console.log (40-4);
+
+//operador de multiplicaçao 
+cosole.log (40*4);
+
+//operador de divisao
+console.log(40/4);
+
+

863c70e7870559b5d31b882b263aadd0f75d1cc9
unknown <system.nathy@hotmail.com>
2021-05-24 18:22:34 -0400

    first commit

---
+++ app3/app3.js
@@ -0,0 +1,23 @@
+// atribuir um valor a uma variavel
+// utilizamos o sinal de IGUAl = para atribuir a essa variavel
+
+nome = "Nathalia";
+
+var sobrenome ="Alves";
+
+var idade = 20;
+
+var salario = 800;
+
+//verificando o que tem dentro das variaveis
+console.log("o nome é " + nome);
+console.log("o sobrenome é: " + sobrenome);
+console.log("a idade é: " + idade);
+console.log("o salario é: " + salario);
+
+//transforma graus celcius em fahrenheit
+//F = c * 1.8 +32
+
+// o grau 20 convertido em fahrenheitt: ===>
+
+console.log("o grau 30 convertido em fahrenheit é: " + (30 *1.8+32));

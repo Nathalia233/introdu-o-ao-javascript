6f7605b2faafd97565cf84ec1bbc69086d98dd66
Nathalia <system.nathy@hotmail.com>
2021-05-25 12:08:29 -0400

    app

---
+++ app11/app11.js
@@ -0,0 +1,15 @@
+// como identificamos se um numero impar ou par?
+
+// %
+
+//pega o 10 e divide por 2, verifica se essa divisao se essa divisao sobrou restos ou nao, caso tenha sobrado ele vai retornar o resto caso nao tenha sobrado ele
+//retorna o zero
+
+var  tipoNumero = null;
+var numero = 11;
+
+if(numero % 2 == 0){
+    console.log("o numero é par!");
+}else{
+    console.log("o numero é impar!");
+}
